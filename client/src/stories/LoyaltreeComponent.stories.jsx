77560f03eb273a3c0c741397fcf890c42dc1fbb9
Logan Woolf <loganwoolf@gmail.com>
2022-03-18 17:17:07 -0600

    Add an example component and stories
    Only the .stories.jsx file needs to be in the components/stories folder

---
+++ client/src/stories/LoyaltreeComponent.stories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import LoyaltreeComponent from './LoyaltreeComponent'
+
+export default {
+  title: 'LoyaltreeComponent',
+  component: LoyaltreeComponent,
+};
+
+//you can put any JS here
+const pText = "Howdy from ptext variable"
+
+
+// Primary is the story name
+export const Primary = () => <LoyaltreeComponent text={pText} />;
+
+export const Secondary = () => <LoyaltreeComponent text="There is no storybook" />;
