6af12c9088f02abf996227a00394ceac3ef1bfac
Casey <caseyjtite@gmail.com>
2022-03-17 19:25:59 -0700

    update seeds for stores

---
+++ server/db/seeds/stores.sql
@@ -0,0 +1,12 @@
+INSERT INTO stores (id, owner_id, name, category, description, photo_url, address, created_at, edited_at)
+VALUES 
+(1,10,'Pizza House', 'Restaurant', 'A Restaurant for all the pizzas','https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg?cs=srgb&dl=pexels-narda-yescas-1566837.jpg&fm=jpg', '1432 store street', default, default )
+(2,7, 'Liberaccis', 'Restaurant', 'Fine dining fantastic view','https://images.pexels.com/photos/1352262/pexels-photo-1352262.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', '2345 Government street', default, default)
+(3,8 , 'Tadpoles Clothing' ,'Retail', 'Home made baby clothes', 'https://images.pexels.com/photos/47220/shoes-pregnancy-child-clothing-47220.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' , '8625 Wharf street',default,default)
+(4,4,'Tounge and Groove Woodworking','Contractor', 'Fine woodworking', 'https://images.pexels.com/photos/6790063/pexels-photo-6790063.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', '3623 Granville street',default,default)
+(5,1,'Johns Mechanics Shop','Auto', 'Small mechanics shop', 'https://images.pexels.com/photos/1108101/pexels-photo-1108101.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', ' 8352 Maple drive',default,default)
+(6,9, 'Auto D-tail', 'Auto', 'Locally owned auto detail shop', 'https://images.pexels.com/photos/372810/pexels-photo-372810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' , '6332 Marine drive',default.default)
+(7,2, 'The Leaky Cauldron' , 'Restaurant', 'Local watering hole','https://images.pexels.com/photos/1267360/pexels-photo-1267360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', '2923 Alder ave',default,default)
+(8,6, 'Late Risers' ,'Restaurant', 'Brunch for all!', 'https://images.pexels.com/photos/2272037/pexels-photo-2272037.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', '2927 Broadway',default,default)
+(9,5, 'Take a Hike' ,'Retail', 'Outdoor gear for every adventure', 'https://images.pexels.com/photos/732632/pexels-photo-732632.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' , '3452 Mountain ave',default,default)
+(10,3, 'Annas Bakery' ,'Restaurant', 'Artisan crafted breads and treats', 'https://images.pexels.com/photos/6152263/pexels-photo-6152263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' , '2342 5th street',default,default)
