ecf3013b450558d962697854ec62fb703c40edc9
Logan Woolf <loganwoolf@gmail.com>
2022-03-21 17:20:13 -0600

    Create stories for GiftCardListItem

--- client/src/stories/GiftCardListItem.stories.jsx
+++ client/src/stories/GiftCardListItem.stories.jsx
@@ -9,7 +9,7 @@ export default {
 
 const props = {
   address: "1432 Road Street",
-  balance: 50,
+  balance: 5525,
   category: "Restaurant",
   description:
     "A Restaurant for all the pizzas and some random flavour text as a test to see if stuff fits into this div",
@@ -25,5 +25,41 @@ const props = {
 };
 
 export const Primary = () => {
+  const props = {
+    address: "1432 Road Street",
+    balance: 5525,
+    category: "Restaurant",
+    description:
+      "A Restaurant for all the pizzas and some random flavour text as a test to see if stuff fits into this div",
+    city: "Vancouver",
+    photo_url:
+      "https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg?cs=srgb&dl=pexels-narda-yescas-1566837.jpg&fm=jpg",
+    point_balance: 3,
+    redeem_at: 10,
+    store_id: 8,
+    store_name: "Pizza House",
+    user_id: 1,
+    card_id: 36,
+  };
+  return <GiftCardListItem key={props.card_id} {...props} />;
+};
+
+export const Second = () => {
+  const props = {
+    address: "665 Westminster Avenue",
+    balance: 5525,
+    category: "Restaurant",
+    description:
+      "A Restaurant for all the pizzas and some random flavour text as a test to see if stuff fits into this div",
+    city: "Vancouver",
+    photo_url:
+      "https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg?cs=srgb&dl=pexels-narda-yescas-1566837.jpg&fm=jpg",
+    point_balance: 0,
+    redeem_at: 0,
+    store_id: 8,
+    store_name: "Pizza House",
+    user_id: 1,
+    card_id: 36,
+  };
   return <GiftCardListItem key={props.card_id} {...props} />;
 };
