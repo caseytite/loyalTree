014370aa92ecb00ccf7fa228574479c6b6b9d108
CBBell99 <chrisbeckertonbell@gmail.com>
2022-03-22 21:41:54 -0700

    add more options to the hamburger menu

--- client/src/Sidebar.jsx
+++ client/src/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { slide as Menu } from 'react-burger-menu';
+import { bubble as Menu } from 'react-burger-menu';
 import "./Sidebar.css";
 
 // import LoggedInUser from '../context/AuthContext';
@@ -21,12 +21,13 @@ export default function Sidebar(props)  {
         Log Out
       </a>}
       
-      <a className="menu-item" href="/register">
+      {!props.user &&<a className="menu-item" href="/register">
         Register
-      </a>
-      <a className="menu-item" href="/desserts">
-        Desserts
-      </a>
+      </a>}
+      {props.user && <a className="menu-item" href="/cards">My Cards</a>} 
+      <a className="menu-item" href="/stores">Browse Stores</a>
+      {props.user.isStoreOwner && <a className="menu-item" href="/dashboard">Dashboard</a>}
+      
     </Menu>
   );
 }; 
