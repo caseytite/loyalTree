62a4eb829b316331dc4c7d8d109ed1bdfba0eeb1
Logan Woolf <loganwoolf@gmail.com>
2022-03-22 23:05:31 -0600

    Physically scan card and check available balance

--- client/src/components/Scanner.jsx
+++ client/src/components/Scanner.jsx
@@ -1,32 +1,45 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
+import axios from "axios";
 import QrScanner from "qr-scanner";
 import Button from "./Button";
 import "./Scanner.css";
 
 const Scanner = (props) => {
   const previewEl = useRef(null);
+  const outputEl = useRef(null);
+  const [cardAmount, setCardAmount] = useState("");
 
-  let qrScanner = null;
+  let qrScanner;
 
-  function scanCode(e) {
-    //event listener function
-    e.preventDefault();
+  const scanCode = () => {
     qrScanner = new QrScanner(previewEl.current, (result) => {
-      if (result) {
-        console.log("decoded qr code:", result);
-        // qrScanner.stop()
-        qrScanner.destroy();
-        // make select query
-      }
+      console.log("decoded qr code:", result);
+      qrScanner.destroy();
+      qrScanner = null;
+
+      // make select query
+      axios
+        .get("/dashboard/redeem", { params: { cardID: result } })
+        .then((response) => {
+          console.log(response.data);
+          setCardAmount(response.data.balance)
+        });
     });
     qrScanner.start();
-  }
+  };
 
   return (
     <div className="scanner">
-      <h2>Scan QR Code Below</h2>
-      <Button onClick={(e) => scanCode(e)} children={"Click to scan"} />
+      <h2>Redeem from Gift Card</h2>
+      <label htmlFor="redeem-amount">
+        Transaction Amount: <input id="redeem-amount" />
+      </label>
+      <p ref={outputEl} id="card-amount">{`Card Amount: $${
+        cardAmount / 100 || "--"
+      }`}</p>
+      {cardAmount && <Button children={"Accept transaction"} />}
+      <Button onClick={() => scanCode()} children={"Click to scan"} />
       <video ref={previewEl}></video>
     </div>
   );
