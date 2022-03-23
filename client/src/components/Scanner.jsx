84f01ecde0f688c0aab9f656ebb4eb02264beacc
Logan Woolf <loganwoolf@gmail.com>
2022-03-23 01:35:59 -0600

    Create post req to finalize redeem transaction
    Receive transaction receipt
    Clear state to default for scanner

--- client/src/components/Scanner.jsx
+++ client/src/components/Scanner.jsx
@@ -8,19 +8,21 @@ import "./Scanner.css";
 const Scanner = (props) => {
   const previewEl = useRef(null);
   const outputEl = useRef(null);
-  const [cardAmount, setCardAmount] = useState(null);
+  const [cardAmt, setCardAmt] = useState(null);
   const [error, setError] = useState(null);
+  const [transAmt, setTransAmt] = useState("");
+  const [cardID, setCardID] = useState(null);
 
   let qrScanner;
 
   const scanCode = () => {
     setError(null);
-    setCardAmount(null);
+    setCardAmt(null);
     qrScanner = new QrScanner(previewEl.current, (result) => {
       console.log("decoded qr code:", result);
       qrScanner.destroy();
       qrScanner = null;
-
+      setCardID(result);
       // make select query
       axios
         .get("/dashboard/redeem", { params: { cardID: result } })
@@ -28,24 +30,48 @@ const Scanner = (props) => {
           console.log(response.data);
           return response.data.error
             ? setError(response.data.error)
-            : setCardAmount(response.data.balance);
+            : setCardAmt(response.data.balance);
         });
     });
     qrScanner.start();
+  };
+
+  const acceptTransaction = () => {
+    axios
+      .post("/dashboard/redeem", {
+        cardID,
+        transAmt,
+        cardAmt,
+      })
+      .then((response) => {
+        console.log("Success!");
+          console.log("post response", response.data);
+          setCardAmt(null);
+          setError(null);
+          setTransAmt("");
+          setCardID(null);
+      });
   };
 
   return (
     <div className="scanner">
       <h2>Redeem from Gift Card</h2>
       <label htmlFor="redeem-amount">
-        Transaction Amount: <input id="redeem-amount" />
+        Transaction Amount:{" "}
+        <input
+          value={transAmt}
+          onChange={(e) => setTransAmt(e.target.value)}
+          id="redeem-amount"
+        />
       </label>
       <p ref={outputEl} id="card-amount">{`Card Amount: $${
-        cardAmount / 100 || "--"
+        cardAmt / 100 || "--"
       }`}</p>
-      {cardAmount && <Button children={"Accept transaction"} />}
+      <Button onClick={scanCode} children={"Click to scan"} />
+      {cardAmt && (
+        <Button onClick={acceptTransaction} children={"Accept transaction"} />
+      )}
       {error && <p>{error}</p>}
-      <Button onClick={() => scanCode()} children={"Click to scan"} />
       <video ref={previewEl}></video>
     </div>
   );
