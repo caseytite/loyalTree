50b01adfa6cfa173ce1ce1a20830736c1041476b
Logan Woolf <loganwoolf@gmail.com>
2022-03-24 17:32:09 -0600

    Redesign layout of landing page

--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -1,52 +1,60 @@
-
 import "./landing.css";
 
 const Landing = (props) => {
   return (
-    <div className="landing-container">
-      <div
-        id="landing-hero"
-        style={{
-          backgroundImage:
-            "url(images/pexels-kaique-rocha-331990.jpg)",
-          // client/public/images/pexels-tim-douglas-6205471.jpg
-        }}
-      ></div>
-      <div id="entry-point">
-        <button>Log In</button>
-        <button>Register</button>
-      </div>
-      <div className="landing-item landing-first">
-        <div className="landing-image" style={{
-          backgroundImage:
-            "url(images/pexels-angela-roma-7319111.jpg)",
-          // client/public/images/pexels-tim-douglas-6205471.jpg
-        }}></div>
-        <div className="landing-message">
-          <p>Gifting made easy</p>
+    <>
+      <header>
+        <div className="button-row">
+          <button>Sign In</button>
+          <button>Register</button>
+        </div>
+        <img src="loyaltree-large.svg" alt="loyaltree logo" />
+        <h2>Gift cards made easy</h2>
+      </header>
+
+      <div className="landing-container">
+
+
+        <div className="image-group">
+          <img src="./images/orange-bag.jpg" alt="" />
+          <article className="article-color-1" >
+            <h2>Gifting made easy</h2>
+            <p>Send love to friends and family while supporting local business.</p>
+          </article>
+          <img src="./images/checkout-guy.jpg" alt="man using loyaltree" />
+          <img src="./images/nice-lady-shopping.jpg" alt="man using loyaltree" />
+        </div>
+
+
+        <div className="image-group">
+        <article className="article-color-3">
+            <h2>Always within reach</h2>
+            <p>All your cards and purchase info, right in your pocket.</p>
+          </article>
+          <img src="./images/ladies-shopping-2.jpg" alt="" />
+          <img src="./images/tablet-guy.jpg" alt="" />
+          <img src="./images/girl-shopping-flowers.jpg" alt="girl shopping" />
+        </div>
+
+        
+        <div className="image-group">
+          <img src="./images/girl-shopping-tiles.jpg" alt="girl shopping" />
+          <img src="./images/open-storefront.jpg" alt="" />
+          <article className="article-color-2">
+            <h2>Loyalty done right</h2>
+            <p>Connect with customers and build lasting relationships.</p>
+          </article>
+          <img src="./images/lady-shopping.jpg" alt="" />
+          
+          
+          {/* 
+          <img src="./images/girl-shopping-back.jpg" alt="girl shopping" />
+          <img src="./images/taking-pic-product.jpg" alt="" /> 
+          */}
+
         </div>
       </div>
-      <div className="landing-item landing-second">
-        <div className="landing-image" style={{
-          backgroundImage:
-            "url(images/pexels-cottonbro-3171000.jpg)",
-          // client/public/images/pexels-tim-douglas-6205471.jpg
-        }}></div>
-        <div className="landing-message">
-          <p>Always within reach</p>
-        </div>
-      </div>
-      <div className="landing-item landing-third">
-        <div className="landing-image" style={{
-          backgroundImage:
-            "url(images/pexels-amina-filkins-5414046.jpg)",
-          // client/public/images/pexels-tim-douglas-6205471.jpg
-        }}></div>
-        <div className="landing-message">
-          <p>Loyalty done right</p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
