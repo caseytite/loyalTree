8d33d24e4ad4ed70fa56dc9c744d4f04647b99f2
CBBell99 <chrisbeckertonbell@gmail.com>
2022-03-24 16:57:05 -0700

    change images to business cards

--- server/db/seeds/02_stores.sql
+++ server/db/seeds/02_stores.sql
@@ -1,13 +1,13 @@
 INSERT INTO stores ( owner_id, name, category, description, photo_url, address, created_at, edited_at)
 VALUES 
-(10,'Pizza House', 'Restaurant', 'A Restaurant for all the pizzas','https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg?cs=srgb&dl=pexels-narda-yescas-1566837.jpg&fm=jpg', '1432 store street', default, default ),
-(7, 'Liberaccis', 'Restaurant', 'Fine dining fantastic view','https://images.pexels.com/photos/1352262/pexels-photo-1352262.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', '2345 Government street', default, default),
-(8 , 'Tadpoles Clothing' ,'Retail', 'Home made baby clothes', 'https://images.pexels.com/photos/47220/shoes-pregnancy-child-clothing-47220.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' , '8625 Wharf street',default, default),
-(4,'Italianos','Restaurant', 'Pasta of all sorts', 'https://images.pexels.com/photos/1438672/pexels-photo-1438672.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', '3623 Granville street',default, default),
-(1,'Johns Mechanics','Auto', 'Small mechanics shop', 'https://images.pexels.com/photos/1108101/pexels-photo-1108101.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', ' 8352 Maple drive',default, default),
-(9, 'Auto D-tail', 'Auto', 'Locally owned auto detail shop', 'https://images.pexels.com/photos/372810/pexels-photo-372810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' , '6332 Marine drive',default, default),
-(2, 'The Leaky Cauldron' , 'Restaurant', 'Local watering hole','https://images.pexels.com/photos/1267360/pexels-photo-1267360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', '2923 Alder ave',default, default),
-(6, 'Late Risers' ,'Restaurant', 'Brunch for all!', 'https://images.pexels.com/photos/2272037/pexels-photo-2272037.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', '2927 Broadway',default, default),
-(5, 'Take a Hike' ,'Retail', 'Outdoor gear for every adventure', 'https://images.pexels.com/photos/732632/pexels-photo-732632.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' , '3452 Mountain ave',default, default),
-(11, 'Annas Bakery' ,'Restaurant', 'Artisan crafted breads and treats', 'https://images.pexels.com/photos/6152263/pexels-photo-6152263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' , '2342 5th street', default,default)
+(10,'Paws Pet Shop', 'Retail', 'A Restaurant for all the pizzas','https://imgur.com/6fZTX3U.gif', '1432 store street', default, default ),
+(7, 'Hook Seabar', 'Restaurant', 'Fine dining fantastic view','https://imgur.com/3sSWmJk.gif', '2345 Government street', default, default),
+(8 , 'Fox Clothing Co.' ,'Retail', 'Home made baby clothes', 'https://imgur.com/qeX5Q5f.gif' , '8625 Wharf street',default, default),
+(4,'Nobu Sushi','Restaurant', 'Pasta of all sorts', 'https://imgur.com/rB4IGYm.gif', '3623 Granville street',default, default),
+(1,'Yes Sir Auto Shop','Auto', 'Small mechanics shop', 'https://imgur.com/9jAYTbK.gif', ' 8352 Maple drive',default, default),
+(9, 'The Horizon Closet', 'Retail', 'Locally owned auto detail shop', 'https://imgur.com/J0gmk6r.gif' , '6332 Marine drive',default, default),
+(2, 'The Leaky Cauldron' , 'Restaurant', 'Local watering hole','https://imgur.com/CLoKIRs.gif', '2923 Alder ave',default, default),
+(6, 'The Roasted Bean' ,'Restaurant', 'Brunch for all!', 'https://imgur.com/kQR76M6.gif', '2927 Broadway',default, default),
+(5, 'Full Swing Pro Shop' ,'Retail', 'Outdoor gear for every adventure', 'https://imgur.com/XvpGZ3d.gif' , '3452 Mountain ave',default, default),
+(11, 'Annas Bakery' ,'Restaurant', 'Artisan crafted breads and treats', 'https://i.imgur.com/yM1Y6Ob.gif' , '2342 5th street', default,default)
 ;
