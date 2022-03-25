d7ded72babf73826a7bf20abc5de4981ba75c26c
Logan Woolf <loganwoolf@gmail.com>
2022-03-25 15:01:12 -0600

    Add some cute stuff to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Footer.css";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = (props) => {
   return (
@@ -9,30 +9,47 @@ const Footer = (props) => {
       <div className="footer-links">
         <div className="footer-column">
           <h2>Social</h2>
-          <p><FontAwesomeIcon icon="fa-brands fa-twitter" /> Twitter</p>
-          <p><FontAwesomeIcon icon="fa-brands fa-facebook" /> Facebook</p>
-          <p><FontAwesomeIcon icon="fa-brands fa-instagram" /> Instagram</p>
+          <a href="https://twitter.com">
+            <FontAwesomeIcon icon="fa-brands fa-twitter" /> Twitter
+          </a>
+          <a href="https://facebook.com">
+            <FontAwesomeIcon icon="fa-brands fa-facebook" /> Facebook
+          </a>
+          <a href="https://instagram.com">
+            <FontAwesomeIcon icon="fa-brands fa-instagram" /> Instagram
+          </a>
         </div>
         <div className="footer-column">
           <h2>About Us</h2>
-          <p>Who we are</p>
-          <p>Program Info</p>
+          <a href="/">Who we are</a>
+          <a href="/">Program Info</a>
         </div>
         <div className="footer-column">
           <h2>Creators</h2>
-          <p><FontAwesomeIcon icon="fa-brands fa-github" /> Casey Tite</p>
-          <p><FontAwesomeIcon icon="fa-brands fa-github" /> Chris Bell</p>
-          <p><FontAwesomeIcon icon="fa-brands fa-github" /> Logan Woolf</p>
+          <a href="https://github.com/caseytite">
+            <FontAwesomeIcon icon="fa-brands fa-github" />
+            Casey Tite
+          </a>
+          <a href="https://github.com/CBBell99">
+            <FontAwesomeIcon icon="fa-brands fa-github" />
+            Chris Bell
+          </a>
+          <a href="https://github.com/loganwoolf">
+            <FontAwesomeIcon icon="fa-brands fa-github" />
+            Logan Woolf
+          </a>
         </div>
       </div>
-      <p className="thank-you">
-        A very special thank you to our families who supported us all the way
-        through and helped with ideas and graphics for this project.
-      </p>
-      <p className="thank-you">
-        Another special thanks to all of the instructors, mentors, and staff at
-        Lighthouse Labs! You are amazing and have changed our lives!
-      </p>
+      <div className="thank-you">
+        <p>
+          A very special thank you to our families who supported us all the way
+          through and helped with ideas and graphics for this project.
+        </p>
+        <p>
+          Another special thanks to all of the instructors, mentors, and staff
+          at Lighthouse Labs! You are amazing and have changed our lives!
+        </p>
+      </div>
     </footer>
   );
 };
