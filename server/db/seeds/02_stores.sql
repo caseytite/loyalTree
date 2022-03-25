b73c4628464dba3237a16f14ba4205117bc6c9ef
CBBell99 <chrisbeckertonbell@gmail.com>
2022-03-24 18:18:38 -0700

    minute style changes

--- server/db/seeds/02_stores.sql
+++ server/db/seeds/02_stores.sql
@@ -1,13 +1,13 @@
 INSERT INTO stores ( owner_id, name, category, description, photo_url, address, created_at, edited_at)
 VALUES 
-(10,'Paws Pet Shop', 'Retail', 'A Restaurant for all the pizzas','https://imgur.com/6fZTX3U.gif', '1432 store street', default, default ),
-(7, 'Hook Seabar', 'Restaurant', 'Fine dining fantastic view','https://imgur.com/3sSWmJk.gif', '2345 Government street', default, default),
+(10,'Paws Pet Shop', 'Retail', 'A place for all our fluffy friends needs','https://imgur.com/6fZTX3U.gif', '1432 store street', default, default ),
+(7, 'Hook Seabar', 'Restaurant', 'Seafood and raw bar with a fantastic view','https://imgur.com/3sSWmJk.gif', '2345 Government street', default, default),
 (8 , 'Fox Clothing Co.' ,'Retail', 'Home made baby clothes', 'https://imgur.com/qeX5Q5f.gif' , '8625 Wharf street',default, default),
-(4,'Nobu Sushi','Restaurant', 'Pasta of all sorts', 'https://imgur.com/rB4IGYm.gif', '3623 Granville street',default, default),
-(1,'Yes Sir Auto Shop','Auto', 'Small mechanics shop', 'https://imgur.com/9jAYTbK.gif', ' 8352 Maple drive',default, default),
-(9, 'The Horizon Closet', 'Retail', 'Locally owned auto detail shop', 'https://imgur.com/J0gmk6r.gif' , '6332 Marine drive',default, default),
+(4,'Nobu Sushi','Restaurant', 'Best sushi this side of the Pacific', 'https://imgur.com/rB4IGYm.gif', '3623 Granville street',default, default),
+(1,'Yes Sir Auto Shop','Auto', 'Mechanic shop that takes care of your car and you', 'https://imgur.com/9jAYTbK.gif', ' 8352 Maple drive',default, default),
+(9, 'The Horizon Closet', 'Retail', 'High end luxury clothing store', 'https://imgur.com/J0gmk6r.gif' , '6332 Marine drive',default, default),
 (2, 'The Leaky Cauldron' , 'Restaurant', 'Local watering hole','https://imgur.com/CLoKIRs.gif', '2923 Alder ave',default, default),
-(6, 'The Roasted Bean' ,'Restaurant', 'Brunch for all!', 'https://imgur.com/kQR76M6.gif', '2927 Broadway',default, default),
-(5, 'Full Swing Pro Shop' ,'Retail', 'Outdoor gear for every adventure', 'https://imgur.com/XvpGZ3d.gif' , '3452 Mountain ave',default, default),
+(6, 'The Roasted Bean' ,'Restaurant', 'Locally owned coffee shop', 'https://imgur.com/kQR76M6.gif', '2927 Broadway',default, default),
+(5, 'Full Swing Pro Shop' ,'Retail', 'For golfers, by golfers ', 'https://imgur.com/XvpGZ3d.gif' , '3452 Mountain ave',default, default),
 (11, 'Annas Bakery' ,'Restaurant', 'Artisan crafted breads and treats', 'https://i.imgur.com/yM1Y6Ob.gif' , '2342 5th street', default,default)
 ;
