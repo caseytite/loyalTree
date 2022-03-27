61d7dca02f23763b668e0257e0dcdbe86c2e66f5
Logan Woolf <loganwoolf@gmail.com>
2022-03-26 18:30:56 -0600

    Move dashboard transactions into dashboard cont

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Button from "../components/Button";
 import Scanner from "../components/Scanner";
-import { useNavigate } from "react-router-dom";
 import TransactionListItem from "../components/TransactionListItem";
 import "./dashboard.css";
 
@@ -18,42 +16,45 @@ const Dashboard = (props) => {
       setTransactions(values[1].data.data);
     });
     console.log(storeInfo);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  
+
   console.log(transactions);
   // while building dashboard
   // console.log(storeInfo);
 
   return (
-    <>
-      <div className="dashboard">
-        <h2>{`Dashboard » ${storeInfo.name}`}</h2>
-        <Scanner />
-      </div>
-      {transactions.length> 1 ? <section className="transactions-table">
-        <h2>Transaction History</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Transaction Number</th>
-              <th>Amount</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-          {transactions.map((transaction) => (
-            <TransactionListItem
-              key={transaction.id}
-              id={transaction.id}
-              amount={Math.abs(transaction.amount)}
-              storeId={transaction.store_id}
-              date={transaction.created_at}
-            />
-          ))}
-          </tbody>
-        </table>
-      </section>: <h2>No transaction history</h2>}
-    </>
+    <div className="dashboard">
+      <h2>{`Dashboard » ${storeInfo.name}`}</h2>
+      <Scanner />
+      {transactions.length > 1 ? (
+        <section className="transactions-table">
+          <h2>Transaction History</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Transaction Number</th>
+                <th>Amount</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map((transaction) => (
+                <TransactionListItem
+                  key={transaction.id}
+                  id={transaction.id}
+                  amount={Math.abs(transaction.amount)}
+                  storeId={transaction.store_id}
+                  date={transaction.created_at}
+                />
+              ))}
+            </tbody>
+          </table>
+        </section>
+      ) : (
+        <h2>No transaction history</h2>
+      )}
+    </div>
   );
 };
 
