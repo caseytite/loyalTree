1d08c8310fae4a45d89a88f1f3a40092d50876ad
Logan Woolf <loganwoolf@gmail.com>
2022-03-27 01:59:06 -0600

    Clean up unused routes and variables from server

--- server/server.js
+++ server/server.js
@@ -16,7 +16,6 @@ const db = new Pool(dbParams);
 db.connect();
 
 const app = express();
-// const router = express.Router()
 
 app.use(morgan("dev"));
 app.use(cors());
@@ -37,23 +36,9 @@ app.use("/dashboard", dashboardRoutes(db));
 const storeRoutes = require("./routes/stores");
 app.use("/stores", storeRoutes(db));
 
-const {
-  USER,
-  USERS,
-  STORES,
-  ADD_USER,
-  TRANSACTIONS,
-  USERS_STORES, // most likely not needed
-  STORE_TRANSACTIONS,
-} = require("./querys");
+const { USER } = require("./querys");
 
 // ---------------------USERS ----------------------
-// not being used
-app.get("/users", (req, res) => {
-  db.query(USERS)
-    .then((data) => res.json({ data: data.rows }))
-    .catch((err) => res.json({ error: err.message }));
-});
 
 app.get("/user", (req, res) => {
   db.query("select * from users where users.id = $1", [req.session.id]).then(
@@ -78,47 +63,6 @@ app.post("/login", (req, res) => {
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/");
-});
-
-// not being used
-app.post("/users", (req, res) => {
-  const [query, params] = ADD_USER(req.body);
-  db.query(query, params)
-    .then((data) => res.json({ data: data.rows }))
-    .catch((err) => console.log("error", err.message));
-});
-
-//------------TRANSACTIONS // none of these are being used
-// ---transactions specific to a store and user
-
-// not being used
-app.get("/transactions/:store/:user", (req, res) => {
-  db.query(
-    `SELECT * FROM transactions
-JOIN stores ON store_id = stores.id
-JOIN users ON owner_id = users.id
-where users.id = $2 AND store_id = $1
-ORDER BY transactions.created_at ASC
-`,
-    [req.params.store, req.params.user]
-  )
-    .then((data) => res.json({ data: data.rows }))
-    .catch((err) => res.json({ error: err.message }));
-});
-
-// not being used
-app.get("/transactions", (req, res) => {
-  db.query(TRANSACTIONS)
-    .then((data) => res.json({ data: data.rows }))
-    .catch((err) => res.json({ error: err.message }));
-});
-
-// not being used
-app.get("/store/transactions", (req, res) => {
-  const [query, params] = STORE_TRANSACTIONS();
-  db.query(query)
-    .then((data) => res.json({ data: data.rows }))
-    .catch((err) => res.json({ error: err.message }));
 });
 
 // redeem points for a gift card
