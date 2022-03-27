2d8412b6b173ef30cabba01882ae1cff45caea83
Logan Woolf <loganwoolf@gmail.com>
2022-03-27 17:48:04 -0600

    Seed Table story with transaction data

--- client/src/stories/Table.stories.jsx
+++ client/src/stories/Table.stories.jsx
@@ -5,7 +5,22 @@ export default {
   component: Table,
 };
 
-export const Primary = (props) => {
+const transactions = [{
+  "id": 62,
+  "giftcard_id": 12,
+  "store_id": 4,
+  "amount": -600,
+  "receiver_id": null,
+  "created_at": "2022-03-28T01:34:08.604Z"
+},{
+  "id": 60,
+  "giftcard_id": 10,
+  "store_id": 4,
+  "amount": -1250,
+  "receiver_id": null,
+  "created_at": "2022-03-27T02:44:08.604Z"
+}]
 
-  return <Table />
+export const Primary = (props) => {
+  return <Table tableData={transactions} />
 }
