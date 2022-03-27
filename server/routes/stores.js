f28e7d84b42f7700a31e16241c26f9d6ad3f8c08
Logan Woolf <loganwoolf@gmail.com>
2022-03-27 01:53:16 -0600

    Enable stores route

--- server/routes/stores.js
+++ server/routes/stores.js
@@ -1,8 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+
 module.exports = (db) => {
-  
+  router.get("/", (req, res) => {
+    db.query("SELECT * FROM stores")
+      .then((data) => res.json({ data: data.rows }))
+      .catch((err) => res.json({ error: err.message }));
+  });
+
+  router.get("/:id", (req, res) => {
+    db.query(
+      `SELECT * FROM stores
+       WHERE id = $1;`,
+      [req.params.id]
+    )
+
+      .then((data) => res.json({ data: data.rows }))
+      .catch((err) => res.json({ error: err.message }));
+  });
 
   return router;
-}
+};
