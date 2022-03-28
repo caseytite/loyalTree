feeaf49c549b17cc511b92bc38280a478ec1beec
CBBell99 <chrisbeckertonbell@gmail.com>
2022-03-28 10:32:56 -0700

    remove nonreferenced files

--- client/src/components/CardTransactions.jsx
+++ client/src/components/CardTransactions.jsx
@@ -7,29 +7,12 @@ import "./Table.css";
 const CardTransactions = (props) => {
   const [transactions, setTransactions] = useState([]);
 
-  const formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  });
-
   useEffect(() => {
     axios
       .get(`/cards/${props.cardID}/transactions`)
       .then((response) => setTransactions(response.data));
   }, [props.cardID]);
 
-  const transactionTable = transactions.map((data) => {
-    const formattedDate = new Date(data.created_at);
-    return (
-      
-      <tr key={data.id}> 
-        <td>{data.id}</td>
-        <td>{formatter.format(data.amount / 100)}</td>
-        <td>{formattedDate.toDateString()}</td>
-      </tr>
-    );
-    
-  });
   return (
     <section className="transaction-container">
       <h2>Transaction History</h2>
