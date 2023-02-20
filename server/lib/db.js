f66883c3fe7724497b6db346c828a17db089d1d9
CBBell99 <chrisbeckertonbell@gmail.com>
2023-02-19 23:03:23 -0800

    change env variables

--- server/lib/db.js
+++ server/lib/db.js
@@ -3,11 +3,16 @@ if (process.env.DATABASE_URL) {
   dbParams.connectionString = process.env.DATABASE_URL;
 } else {
   dbParams = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_NAME
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
   };
 }
 
